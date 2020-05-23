894093e6cb6b6c2598a323752c166400a02c8afb
Filipe Alves <filipe.alvesdsouza@gmail.com>
2020-05-23 12:50:22 -0300

    Atualização do layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,279 +1,284 @@
-const tabelaTeclas = document.getElementById('tabelaTeclas');
-const resultado = document.getElementById('resultado');
-const operadores = ["+", "-", "÷", "×"];
-let contador = 0;
+const keyboard = document.getElementById('keyboard');
+const result = document.getElementById('result');
+const operators = ["+", "-", "÷", "×"];
+let counter = 0;
 
-/* VERIFICA SE O DIGITO É UM OPERADOR OU PONTO */
-function checarDigito(digito) {
-    const tamanhoResultado = resultado.textContent.length;
-    const ultimoDigito = resultado.textContent.substring(tamanhoResultado - 1, tamanhoResultado);
-    let statusPonto = true;
+function checkDigit(digit) {
+	const resultSize = result.textContent.length;
+	const lastDigit = result.textContent.substring(resultSize - 1, resultSize);
+	let dotStatus = true;
 
-    /* DIGITO SENDO UM 'PONTO' */
-    if (digito === ".") {
-        contador = 0;
-        let posicao;
-        for (let i = 0; i < operadores.length; i++) {
-            for (let j = 1; j < tamanhoResultado; j++) {
-                if (resultado.textContent.charAt(j) === operadores[i]) {
-                    contador++;
-                    posicao = j;
-                }
-            }
-        }
+	if (digit === ".") {
+		counter = 0;
+		let position;
+		for (let i = 0; i < operators.length; i++) {
+			for (let j = 1; j < resultSize; j++) {
+				if (result.textContent.charAt(j) === operators[i]) {
+					counter++;
+					position = j;
+				}
+			}
+		}
 
-        if (contador === 0) { // Está digitando o primeiro número (n1)
-            for (let i = 0; i < tamanhoResultado; i++) {
-                if (resultado.textContent.charAt(i) === ".") {
-                    statusPonto = false;
-                }
-            }
-        } else { // Está digitando o segundo número (n2)
-            for (let i = posicao + 1; i < tamanhoResultado; i++) {
-                if (resultado.textContent.charAt(i) === ".") {
-                    statusPonto = false;
-                }
-            }
-        }
+		if (counter === 0) {
+			for (let i = 0; i < resultSize; i++) {
+				if (result.textContent.charAt(i) === ".") {
+					dotStatus = false;
+				}
+			}
+		} else {
+			for (let i = position + 1; i < resultSize; i++) {
+				if (result.textContent.charAt(i) === ".") {
+					dotStatus = false;
+				}
+			}
+		}
 
-        if (tamanhoResultado === 0) {
-            resultado.innerText = `0.`;
-        } else if (statusPonto) {
-            contador = 0;
+		if (resultSize === 0) {
+			result.innerText = `0.`;
+		} else if (dotStatus) {
+			counter = 0;
 
-            operadores.forEach(operador => {
-                if (ultimoDigito === operador) {
-                    contador++;
-                }
-            });
+			operators.forEach(operator => {
+				if (lastDigit === operator) {
+					counter++;
+				}
+			});
 
-            if (contador === 0) {
-                resultado.innerText += `.`;
-            } else {
-                resultado.innerText += `0.`;
-            }
-        }
-    }
+			if (counter === 0) {
+				result.innerText += `.`;
+			} else {
+				result.innerText += `0.`;
+			}
+		}
+	}
 
-    /* DIGITO SENDO UM 'OPERADOR' */
-    operadores.forEach(operador => {
-        // verifica se o dígito é um operador
-        if (digito === operador && ultimoDigito !== ".") {
-            // verifica se está no início ou no meio do cálculo
-            if (tamanhoResultado === 0) {
-                if (digito === operadores[0] || digito === operadores[1]) {
-                    resultado.innerText += digito; // adiciona '+' ou '-'
-                }
-            } else {
-                contador = 0;
-                // verifica se o último dígito é um operador
-                operadores.forEach(operador => {
-                    if (ultimoDigito === operador) {
-                        contador++;
-                    }
-                });
+	operators.forEach(operator => {
+		if (digit === operator && lastDigit !== ".") {
+			if (resultSize === 0) {
+				if (digit === operators[0] || digit === operators[1]) {
+					result.innerText += digit;
+				}
+			} else {
+				counter = 0;
+				operators.forEach(operator => {
+					if (lastDigit === operator) {
+						counter++;
+					}
+				});
 
-                if (contador === 0) { // último dígito não é um operador
-                    for (let i = 0; i < operadores.length; i++) {
-                        for (let j = 1; j < tamanhoResultado; j++) {
-                            if (resultado.textContent.charAt(j) === operadores[i]) {
-                                calcular();
-                            }
-                        }
-                    }
+				if (counter === 0) {
+					for (let i = 0; i < operators.length; i++) {
+						for (let j = 1; j < resultSize; j++) {
+							if (result.textContent.charAt(j) === operators[i]) {
+								calc();
+							}
+						}
+					}
 
-                    resultado.innerText += digito;
-                } else { // último dígito é um operador
-                    apagar("clearError"); // apaga o operador antigo
-                    resultado.innerText += digito; // acrescenta o operador atual (dígito)
-                }
-            }
-        }
-    });
+					result.innerText += digit;
+				} else {
+					if (resultSize === 1) {
+						if (digit === operators[0] || digit === operators[1]) {
+							clean("error");
+							result.innerText += digit;
+						}
+					} else {
+						clean("error");
+						result.innerText += digit;
+					}
+				}
+			}
+		}
+	});
 }
 
-/* EXIBE O DIGITO NA TELA DE RESULTADO */
-function exibir(digito, checar) {
-    const tamanhoResultado = resultado.textContent.length;
+function show(digit, check) {
+	const resultSize = result.textContent.length;
 
-    if (tamanhoResultado <= 11) {
-        if (checar) {
-            checarDigito(digito);
-        } else {
-            resultado.innerText += digito;
-        }
-    }
+	if (result.textContent === "Impossivel") {
+		clean("all");
+	}
+
+	if (resultSize <= 10) {
+		if (check) {
+			checkDigit(digit);
+		} else {
+			result.innerText += digit;
+		}
+	}
 }
 
-/* APAGA O ÚLTIMO DIGITO OU TODO O CÁLCULO */
-function apagar(porcao) {
-    if (resultado.textContent.length > 0) {
-        if (porcao === "clear") {
-            resultado.innerText = "";
-        } else if (porcao === "clearError") {
-            resultado.innerText = resultado.textContent.substring(0, resultado.textContent.length - 1);
-        }
-    }
+function clean(section) {
+	if (result.textContent.length > 0) {
+		if (section === "all") {
+			result.innerText = "";
+		} else if (section === "error") {
+			if (result.textContent === "Impossivel") {
+				clean("all");
+			} else {
+				result.innerText = result.textContent.substring(0, result.textContent.length - 1);
+			}
+		}
+	}
 }
 
-/* REALIZA O CÁLCULO (n1 op n2 = r) */
-function calcular() {
-    const tamanhoResultado = resultado.textContent.length;
+function calc() {
+	const resultSize = result.textContent.length;
 
-    let n1;
-    let op;
-    let n2;
-    let r;
+	let n1;
+	let op;
+	let n2;
+	let r;
 
-    for (let i = 0; i < operadores.length; i++) {
-        for (let j = 1; j < tamanhoResultado; j++) {
-            if (resultado.textContent.charAt(j) === operadores[i]) {
-                n1 = Number(resultado.textContent.substring(0, j));
-                op = operadores[i];
-                n2 = Number(resultado.textContent.substring(j + 1, tamanhoResultado));
+	for (let i = 0; i < operators.length; i++) {
+		for (let j = 1; j < resultSize; j++) {
+			if (result.textContent.charAt(j) === operators[i]) {
+				n1 = Number(result.textContent.substring(0, j));
+				op = operators[i];
+				n2 = Number(result.textContent.substring(j + 1, resultSize));
 
-                switch (op) {
-                    case "+":
-                        r = n1 + n2;
-                        break;
+				switch (op) {
+					case "+":
+						r = n1 + n2;
+						break;
 
-                    case "-":
-                        r = n1 - n2;
-                        break;
+					case "-":
+						r = n1 - n2;
+						break;
 
-                    case "÷":
-                        r = n1 / n2;
-                        break;
+					case "÷":
+						r = n1 / n2;
+						break;
 
-                    case "×":
-                        r = n1 * n2;
-                        break;
+					case "×":
+						r = n1 * n2;
+						break;
 
-                    default:
-                        break;
-                }
+					default:
+						break;
+				}
 
-                // limita o resultado à 5 caracteres
-                if (r.toString().length > 5) {
-                    for (let i = 0; i < r.toString().length; i++) {
-                        if (r.toString().charAt(i) === ".") {
-                            r = r.toString().substring(0, i + 4);
-                        }
-                    }
-                }
+				if (r.toString().length > 5) {
+					for (let i = 0; i < r.toString().length; i++) {
+						if (r.toString().charAt(i) === ".") {
+							r = r.toString().substring(0, i + 4);
+						}
+					}
+				}
 
-                apagar("clear");
+				clean("all");
 
-                const rString = r.toString().toLowerCase();
+				const rString = r.toString().toLowerCase();
 
-                if(rString === "infinity" || rString === "nan" || rString === "-nan" || rString === "-infinity"){
-                    r = "Impossivel";
-                }
-                
-                resultado.innerText = `${r}`;
-            }
-        }
-    }
+				if (rString === "infinity" || rString === "nan" || rString === "-nan" || rString === "-infinity") {
+					r = "Impossivel";
+				}
+
+				result.innerText = `${r}`;
+			}
+		}
+	}
 }
 
-/* REALIZA AS OPERAÇÕES DINAMICAMENTE */
-tabelaTeclas.addEventListener("click", (event) => {
-    const elemento = event.target; // elemento HTML
+keyboard.addEventListener("click", (event) => {
+	const element = event.target;
+	const job = element.dataset.job;'job'
 
-    switch (elemento.dataset.nome) {
-        case "clear":
-            apagar("clear");
-            break;
+	switch (job) {
+		case "clear":
+			clean("all");
+			break;
 
-        case "clearError":
-            apagar("clearError");
-            break;
+		case "clearError":
+			clean("error");
+			break;
 
-        case "numero":
-            exibir(elemento.textContent, false);
-            break;
+		case "number":
+			show(element.textContent, false);
+			break;
 
-        case "operador":
-            exibir(elemento.textContent, true);
-            break;
+		case "operator":
+			show(element.textContent, true);
+			break;
 
-        case "igual":
-            for (let i = 0; i < operadores.length; i++) {
-                for (let j = 1; j < resultado.textContent.length; j++) {
-                    if (resultado.textContent.charAt(j) === operadores[i]) {
-                        calcular();
-                    }
-                }
-            }
-            break;
+		case "equal":
+			for (let i = 0; i < operators.length; i++) {
+				for (let j = 1; j < result.textContent.length; j++) {
+					if (result.textContent.charAt(j) === operators[i]) {
+						calc();
+					}
+				}
+			}
+			break;
 
-        case "ponto":
-            exibir(elemento.textContent, true);
-            break;
+		case "dot":
+			show(".", true);
+			break;
 
-        default:
-            break;
-    }
+		default:
+			break;
+	}
 });
 
 document.addEventListener("keydown", (event) => {
-    const numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    const ponto = [".", ","];
-    const ops = ["+", "-", "*", "/"];
-    const valorTecla = event.key;
+	const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	const dot = [".", ","];
+	const opsArray = ["+", "-", "*", "/"];
+	const keyValue = event.key;
 
-    numeros.forEach(numero => {
-        if (valorTecla === numero) {
-            exibir(valorTecla, false);
-        }
-    });
+	numbers.forEach(number => {
+		if (keyValue === number) {
+			show(keyValue, false);
+		}
+	});
 
-    ponto.forEach(p => {
-        if (valorTecla === p) {
-            exibir(".", true);
-        }
-    });
+	dot.forEach(d => {
+		if (keyValue === d) {
+			show(".", true);
+		}
+	});
 
-    ops.forEach(op => {
-        if (valorTecla === op) {
-            switch (op) {
-                case "+":
-                    exibir("+", true);
-                    break;
+	opsArray.forEach(operator => {
+		if (keyValue === operator) {
+			switch (operator) {
+				case "+":
+					show("+", true);
+					break;
 
-                case "-":
-                    exibir("-", true);
-                    break;
+				case "-":
+					show("-", true);
+					break;
 
-                case "*":
-                    exibir("×", true);
-                    break;
+				case "*":
+					show("×", true);
+					break;
 
-                case "/":
-                    exibir("÷", true);
-                    break;
+				case "/":
+					show("÷", true);
+					break;
 
-                default:
-                    break;
-            }
-        }
-    });
+				default:
+					break;
+			}
+		}
+	});
 
-    if (valorTecla.toLowerCase() === "enter") {
-        for (let i = 0; i < operadores.length; i++) {
-            for (let j = 1; j < resultado.textContent.length; j++) {
-                if (resultado.textContent.charAt(j) === operadores[i]) {
-                    calcular();
-                }
-            }
-        }
-    }
+	if (keyValue.toLowerCase() === "enter") {
+		for (let i = 0; i < operators.length; i++) {
+			for (let j = 1; j < result.textContent.length; j++) {
+				if (result.textContent.charAt(j) === operators[i]) {
+					calc();
+				}
+			}
+		}
+	}
 
-    if (valorTecla.toLowerCase() === "backspace") {
-        apagar("clearError");
-    }
+	if (keyValue.toLowerCase() === "backspace") {
+		clean("error");
+	}
 
-    if (valorTecla.toLowerCase() === "delete") {
-        apagar("clear");
-    }
+	if (keyValue.toLowerCase() === "delete") {
+		clean("all");
+	}
 });
